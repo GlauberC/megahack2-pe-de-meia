661f34e9e6f1fe316e1d89fbe0e5cb82d0f901b8
Glauber Carvalho <glaubercarv@gmail.com>
2020-05-04 10:18:04 -0300

    config app

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import './config/ReactotronConfig';
+
+// import { Container } from './styles';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
